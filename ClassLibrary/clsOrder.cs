4f0e536e194cab77c2467e084777cf6e9089b29a
J-kowiak <jakubwojtkowiak5@gmail.com>
2021-02-03 22:05:05 +0000

    Added Some of the test Statements to my branches and created the class for orders

---
+++ ClassLibrary/clsOrder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsOrder
+    {
+        public int OrderID { get; set; }
+        public string ItemName { get; set; }
+        public double Price { get; set; }
+        public DateTime DateOrderMade { get; set; }
+        public bool ItemShipped { get; set; }
+    }
+}
---
+++ Testing2/tstOrder.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing2
+{
+
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            // creates instance of the class we want to create
+            clsOrder anOrder = new clsOrder();
+            Assert.IsNotNull(anOrder);
+
+        }
+
+        [TestMethod]
+        public void OrderIdOK()
+        {
+            clsOrder anOrder = new clsOrder();
+
+            Int32 TestData = 1;
+
+            anOrder.OrderID = TestData;
+            Assert.AreEqual(anOrder.OrderID, TestData);
+        }
+
+        [TestMethod]
+        public void ItemNameOK()
+        {
+            clsOrder anOrder = new clsOrder();
+
+            string TestData = "21b";
+
+            anOrder.ItemName = TestData;
+            Assert.AreEqual(anOrder.ItemName, TestData);
+        }
+
+        [TestMethod]
+        public void PriceOK()
+        {
+            clsOrder anOrder = new clsOrder();
+
+            Double TestData = 1.23;
+
+            anOrder.Price = TestData;
+            Assert.AreEqual(anOrder.Price, TestData);
+        }
+
+        [TestMethod]
+        public void DateOrderMadeOK()
+        {
+            clsOrder anOrder = new clsOrder();
+
+            DateTime TestData = DateTime.Now.Date;
+
+            anOrder.DateOrderMade = TestData;
+            Assert.AreEqual(anOrder.DateOrderMade, TestData);
+        }
+        [TestMethod]
+        public void ItemShippedOK()
+        {
+            clsOrder anOrder = new clsOrder();
+
+            Boolean TestData = true;
+
+            anOrder.ItemShipped = TestData;
+
+            Assert.AreEqual(anOrder.ItemShipped, TestData);
+
+
+        }
+    }
+}
