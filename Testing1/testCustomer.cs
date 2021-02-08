e05f2ea18f8c195001cd9b4506014c996b8e7f50
Billy Parker <P2528439@my365.dmu.ac.uk>
2021-02-08 02:12:59 +0000

    Added testing and class for customer managment

---
+++ Testing1/testCustomer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing1
+{
+    [TestClass]
+    public class testCustomer
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //test to see that it exists
+            Assert.IsNotNull(ACustomer);
+        }
+    }
+}
