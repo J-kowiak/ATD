cec970e368b7b66cdd88b65edeb15876e6bc2f68
Kevin Addae <P2520693@my365.dmu.ac.uk>
2021-02-10 23:50:28 +0000

    Created test for each of the stock attributes.

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public bool Sale_Ready { get; set; }
+        public DateTime NextDelivery { get; set; }
+        public int Quantity { get; set; }
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public int ProductId { get; set; }
     }
 }
--- Testing3/tstStock.cs
+++ Testing3/tstStock.cs
@@ -7,11 +7,74 @@ namespace Testing3
     [TestClass]
     public class tstStock
     {
+        clsStock stock = new clsStock();
         [TestMethod]
         public void InstanceOK()
         {
-            clsStock stock = new clsStock();
+            //Creates an instance
+            
             Assert.IsNotNull(stock);
         }
+        [TestMethod]
+        public void Sale_ReadyPropertyOK()
+        {
+            // creates some data
+            Boolean data = true;
+            // Assigns the data to the property 
+            stock.Sale_Ready = data;
+            // Tests to see if the data is the same
+            Assert.AreEqual(stock.Sale_Ready, data);
+        }
+        [TestMethod]
+        public void NextDeliveryPropertyOK()
+        {
+            // creates some data
+            DateTime data = DateTime.Now.Date;
+            // Assigns the data to the property 
+            stock.NextDelivery = data;
+            // Tests to see if the value is the same
+            Assert.AreEqual(stock.NextDelivery, data);
+        }
+        [TestMethod]
+        public void QuantityPropertyOK()
+        {
+            // creates some data
+            int data = 1;
+            // Assigns the data to the property 
+            stock.Quantity = data;
+            // Tests to see if the value is the same
+            Assert.AreEqual(stock.Quantity, data);
+        }
+        [TestMethod]
+        public void CategoryPropertyOK()
+        {
+            // creates some data
+            String data = "Jumper";
+            // Assigns the data to the property 
+            stock.Category = data;
+            // Tests to see if the value is the same
+            Assert.AreEqual(stock.Category, data);
+        }
+        [TestMethod]
+        public void NamePropertyOK()
+        {
+            // creates some data
+            String data = "Navy Cotton Jumper";
+            // Assigns the data to the property 
+            stock.Name = data;
+            // Tests to see if the value is the same
+            Assert.AreEqual(stock.Name, data);
+        }
+        [TestMethod]
+        public void ProductIdPropertyOK()
+        {
+            // creates some data
+            int data = 251;
+            // Assigns the data to the property 
+            stock.ProductId = data;
+            // Tests to see if the value is the same
+            Assert.AreEqual(stock.ProductId, data);
+        }
+
     }
 }
