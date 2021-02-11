7dc7f92688da6243a34426e89352dd03fdf4175a
Kevin Addae <P2520693@my365.dmu.ac.uk>
2021-02-11 00:59:51 +0000

    Worked on the basic presentation layer

--- AdminSystem/StockDataEntry.aspx.cs
+++ AdminSystem/StockDataEntry.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,5 +11,22 @@ public partial class _1_DataEntry : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //Creates a new instance of stock
+        clsStock stock = new clsStock();
+        // Captures the category 
+        stock.Category = txtCategory.Text;
+        // Stores category in session objecy
+        Session["stock"] = stock;
+        // Navigates to viewer page
+        Response.Redirect("StockViewer");
     }
 }
