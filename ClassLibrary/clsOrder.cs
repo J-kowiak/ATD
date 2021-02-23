a9a9b40e6006e809ff35f88caae6b7f5b57cf010
J-kowiak <jakubwojtkowiak5@gmail.com>
2021-02-23 20:55:40 +0000

    Created the find method in the middle layer, still need to do presentation and Data.

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -4,10 +4,85 @@ namespace ClassLibrary
 {
     public class clsOrder
     {
-        public int OrderID { get; set; }
-        public string ItemName { get; set; }
-        public double Price { get; set; }
-        public DateTime DateOrderMade { get; set; }
-        public bool ItemShipped { get; set; }
+        //private data member for the order id property
+        private Int32 mOrderID;
+        private string mItemName;
+        private double mPrice;
+        private DateTime mDateOrderMade;
+        private bool mItemShipped;
+
+        public int OrderID {
+            get
+            {
+                //this line of code sends the data out of the property
+                return mOrderID;
+            }
+            set
+            {
+                // this line of code allows data into the property
+                mOrderID = value;
+            }
+        }
+        public string ItemName {
+            get
+            {
+                //this line of code sends the data out of the property
+                return mItemName;
+            }
+            set
+            {
+                // this line of code allows data into the property
+                mItemName = value;
+            }
+        }
+
+        public double Price {
+            get
+            {
+                //this line of code sends the data out of the property
+                return mPrice;
+            }
+            set
+            {
+                // this line of code allows data into the property
+                mPrice = value;
+            }
+        }
+        public DateTime DateOrderMade {
+            get
+            {
+                //this line of code sends the data out of the property
+                return mDateOrderMade;
+            }
+            set
+            {
+                // this line of code allows data into the property
+                mDateOrderMade = value;
+            }
+        }
+        public bool ItemShipped {
+            get
+            {
+                //this line of code sends the data out of the property
+                return mItemShipped;
+            }
+            set
+            {
+                // this line of code allows data into the property
+                mItemShipped = value;
+            }
+        }
+
+        public bool Find(int orderID)
+        {
+            //set the private data members to the test data value
+            mOrderID = 321321;
+            mItemName = "Test Item";
+            mPrice = 23.21;
+            mDateOrderMade = Convert.ToDateTime("23/02/2021");
+            mItemShipped = true;
+            //always return true
+            return true;
+        }
     }
 }
--- Testing2/tstOrder.cs
+++ Testing2/tstOrder.cs
@@ -72,5 +72,130 @@ namespace Testing2
 
 
         }
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            //create an instance of the class that we want to create
+            clsOrder anOrder = new clsOrder();
+            //Boolean variable to store the result of the validation
+            Boolean found = false;
+            //create some test data to use with teh method 
+            Int32 OrderID = 321321;
+            //invoke the method
+            found = anOrder.Find(OrderID);
+            //test to see if the result is true
+            Assert.IsTrue(found);
+
+
+
+        }
+        [TestMethod]
+        public void TestOrderIDFound()
+        {
+            //create an instance of the class that we want to create
+            clsOrder anOrder = new clsOrder();
+            //Boolean variable to store the result of the validation
+            Boolean found = false;
+            //boolean variable to record if data is ok (assume it is)
+            Boolean OK = true;
+            //create some test data to use with teh method 
+            Int32 OrderID = 321321;
+            //invoke the method
+            found = anOrder.Find(OrderID);
+            //check the order id
+            if (anOrder.OrderID != 321321)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestItemNameOK()
+        {
+            //create an instance of the class that we want to create
+            clsOrder anOrder = new clsOrder();
+            //Boolean variable to store the result of the validation
+            Boolean found = false;
+            //boolean variable to record if data is ok (assume it is)
+            Boolean OK = true;
+            //create some test data to use with teh method 
+            Int32 OrderID = 321321;
+            //invoke the method
+            found = anOrder.Find(OrderID);
+            //check the order id
+            if (anOrder.ItemName != "Test Item")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestPriceOK()
+        {
+            //create an instance of the class that we want to create
+            clsOrder anOrder = new clsOrder();
+            //Boolean variable to store the result of the validation
+            Boolean found = false;
+            //boolean variable to record if data is ok (assume it is)
+            Boolean OK = true;
+            //create some test data to use with teh method 
+            Int32 OrderID = 321321;
+            //invoke the method
+            found = anOrder.Find(OrderID);
+            //check the order id
+            if (anOrder.Price != 23.21)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestDateOrderMadeOK()
+        {
+            //create an instance of the class that we want to create
+            clsOrder anOrder = new clsOrder();
+            //Boolean variable to store the result of the validation
+            Boolean found = false;
+            //boolean variable to record if data is ok (assume it is)
+            Boolean OK = true;
+            //create some test data to use with teh method 
+            Int32 OrderID = 321321;
+            //invoke the method
+            found = anOrder.Find(OrderID);
+            //check the order id
+            if (anOrder.DateOrderMade != Convert.ToDateTime("23/02/2021"))
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestItemShipped()
+        {
+            //create an instance of the class that we want to create
+            clsOrder anOrder = new clsOrder();
+            //Boolean variable to store the result of the validation
+            Boolean found = false;
+            //boolean variable to record if data is ok (assume it is)
+            Boolean OK = true;
+            //create some test data to use with teh method 
+            Int32 OrderID = 321321;
+            //invoke the method
+            found = anOrder.Find(OrderID);
+            //check the order id
+            if (anOrder.ItemShipped != true)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
     }
 }
