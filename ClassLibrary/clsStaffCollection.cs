684372d2333261236abe8fe213d1dc902ccde2b8
MaxMJH <MaxHarrisMJH@gmail.com>
2021-02-25 22:09:45 +0000

    Created staff collection class, as well as tests for said class.

---
+++ ClassLibrary/clsStaffCollection.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsStaffCollection
+    {
+        private List<clsStaff> staffList;
+
+        public clsStaffCollection()
+        {
+            this.staffList = new List<clsStaff>();
+
+            Int32 index = 0;
+            Int32 recordCount = 0;
+
+            var db = new clsDataConnection();
+
+            db.Execute("sproc_SelectAllTables");
+
+            recordCount = db.Count;
+
+            while (index < recordCount)
+            {
+                var staff = new clsStaff
+                {
+                    ID = Convert.ToInt32(db.DataTable.Rows[index]["staffID"]),
+                    Admin = Convert.ToBoolean(db.DataTable.Rows[index]["admin"]),
+                    DateOfCreation = Convert.ToDateTime(db.DataTable.Rows[index]["dateOfCreation"]),
+                    Address = Convert.ToString(db.DataTable.Rows[index]["staffAddress"]),
+                    Name = Convert.ToString(db.DataTable.Rows[index]["staffName"]),
+                    Password = Convert.ToString(db.DataTable.Rows[index]["staffPassword"]),
+                    Username = Convert.ToString(db.DataTable.Rows[index]["staffUsername"]),
+                    Age = Convert.ToInt32(db.DataTable.Rows[index]["staffAge"])
+                };
+
+                this.staffList.Add(staff);
+
+                index++;
+            }
+        }
+
+        public List<clsStaff> StaffList
+        {
+            get
+            {
+                return this.staffList;
+            }
+
+            set
+            {
+                this.staffList = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.staffList.Count;
+            }
+
+            set
+            {
+                // NA.
+            }
+        }
+
+        public clsStaff ThisStaffMember
+        {
+            get;
+
+            set;
+        }
+    }
+}
---
+++ Testing4/tstStaffCollection.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstStaffCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            var allStaff = new clsStaffCollection();
+
+            Assert.IsNotNull(allStaff);
+        }
+
+        [TestMethod]
+        public void StaffListOK()
+        {
+            var allStaff = new clsStaffCollection();
+
+            List<clsStaff> staffList = new List<clsStaff>();
+
+            var staff = new clsStaff
+            {
+                ID = 2,
+                Admin = false,
+                DateOfCreation = DateTime.Now.Date,
+                Address = "112 Test Road",
+                Name = "Jane Doe",
+                Password = "test321",
+                Username = "JaneDoe@gmail.com",
+                Age = 30
+            };
+
+            staffList.Add(staff);
+
+            allStaff.StaffList = staffList;
+
+            Assert.AreEqual(allStaff.StaffList, staffList);
+        }
+
+        [TestMethod]
+        public void ThisStaffMemberOK()
+        {
+            var allStaff = new clsStaffCollection();
+
+            var staff = new clsStaff
+            {
+                ID = 2,
+                Admin = false,
+                DateOfCreation = DateTime.Now.Date,
+                Address = "112 Test Road",
+                Name = "Jane Doe",
+                Password = "test321",
+                Username = "JaneDoe@gmail.com",
+                Age = 30
+            };
+
+            allStaff.ThisStaffMember = staff;
+
+            Assert.AreEqual(allStaff.ThisStaffMember, staff);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            var allStaff = new clsStaffCollection();
+
+            List<clsStaff> staffList = new List<clsStaff>();
+
+            var staff = new clsStaff
+            {
+                ID = 2,
+                Admin = false,
+                DateOfCreation = DateTime.Now.Date,
+                Address = "112 Test Road",
+                Name = "Jane Doe",
+                Password = "test321",
+                Username = "JaneDoe@gmail.com",
+                Age = 30
+            };
+
+            staffList.Add(staff);
+
+            allStaff.StaffList = staffList;
+
+            Assert.AreEqual(allStaff.Count, staffList.Count);
+        }
+    }
+}
