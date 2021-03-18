b11a13cf03e5b0e0d5821acbcef50a9e20ebab26
KevinAddae <kevinaddae94@gmail.com>
2021-03-18 14:45:32 +0000

    Created the stock collection class and test.

---
+++ ClassLibrary/clsStockCollection.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsStockCollection
+    {
+        List<clsStock> mStockList = new List<clsStock>();
+      
+
+        public List<clsStock> StockList
+        {
+            get
+            {
+                return mStockList;
+            }
+            set
+            {
+                mStockList = value;
+            }
+
+        }
+        public int Count
+        {
+            get
+            {
+                return mStockList.Count;
+            }
+            set
+            {
+
+            }
+
+        }
+
+        public clsStock ThisStock { get; set; }
+
+        public clsStockCollection()
+        {
+            
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            clsDataConnection Db = new clsDataConnection();
+
+            Db.Execute("sproc_tblStock_SelectAll");
+
+            RecordCount = Db.Count;
+
+            while(Index < RecordCount)
+            {
+                clsStock AnStock = new clsStock();
+                AnStock.Name = Convert.ToString(Db.DataTable.Rows[Index]["Name"]);
+                AnStock.Category = Convert.ToString(Db.DataTable.Rows[Index]["Category"]);
+                AnStock.Quantity = Convert.ToInt32(Db.DataTable.Rows[Index]["Quantity"]);
+                AnStock.ProductId = Convert.ToInt32(Db.DataTable.Rows[Index]["ProductId"]);
+                AnStock.NextDelivery = Convert.ToDateTime(Db.DataTable.Rows[Index]["NextDelivery"]);
+                AnStock.Sale_Ready = Convert.ToBoolean(Db.DataTable.Rows[Index]["Sale_Ready"]);
+
+                mStockList.Add(AnStock);
+                Index++;
+            }
+        }
+    }
+}
---
+++ Testing3/tstStockCollection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstStockCollection
+    {
+        clsStockCollection allStock = new clsStockCollection();
+        clsStock ItemTest = new clsStock();
+
+
+        [TestMethod]
+        public void InstanceOk()
+        {
+            Assert.IsNotNull(allStock);
+        }
+        [TestMethod]
+        public void StockListOk()
+        {
+            List<clsStock> TestList = new List<clsStock>();
+
+            ItemTest.Name = "Pink Hat";
+            ItemTest.Category = "HeadWear";
+            ItemTest.Quantity = 70;
+            ItemTest.Sale_Ready = true;
+            ItemTest.NextDelivery = DateTime.Now.Date;
+            ItemTest.ProductId = 1;
+
+            TestList.Add(ItemTest);
+            // Assign data to propety
+            allStock.StockList = TestList;
+            Assert.AreEqual(allStock.StockList, TestList);
+        }
+
+        [TestMethod]
+        public void ThisStockPropertyOk()
+        {
+            clsStock TestStock = new clsStock();
+            TestStock.Name = "Pink Hat";
+            TestStock.Category = "HeadWear";
+            TestStock.Quantity = 70;
+            TestStock.Sale_Ready = true;
+            TestStock.NextDelivery = DateTime.Now.Date;
+            TestStock.ProductId = 1;
+
+            allStock.ThisStock = TestStock;
+            Assert.AreEqual(allStock.ThisStock, TestStock);
+
+        }
+
+        [TestMethod]
+        public void TwoRecordsPresent()
+        {
+            Assert.AreEqual(allStock.Count, 2); 
+
+        }
+    }
+}
