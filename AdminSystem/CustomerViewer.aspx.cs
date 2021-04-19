54c4e884a9468d077f572bb8a108ef78f0a79b0f
Billy Parker <billyeparker@gmail.com>
2021-04-19 01:25:08 +0100

    Added Basically Everything
    +Validation Both Layers
    +Collection Class Both Layers
    +All Collection Methods
    +Added Viewer
    
    I still need to add my tables and procedures to the sql database on the DMU servers however.

--- AdminSystem/CustomerViewer.aspx.cs
+++ AdminSystem/CustomerViewer.aspx.cs
@@ -13,8 +13,20 @@ public partial class _1Viewer : System.Web.UI.Page
         //create a new instance of clsCustomer
         clsCustomer ACustomer = new clsCustomer();
         //get the data from the session object
-        ACustomer = (clsCustomer)Session["ACustomer"];
+        ACustomer.Find((int)Session["SelectedCustomerID"]);
         //display the customer number for this entry
-        Response.Write(ACustomer.Name);
+        Response.Write("CustomerNo: " + ACustomer.CustomerNo + "<br>");
+        Response.Write("Email: " + ACustomer.Email + "<br>");
+        Response.Write("Name: " + ACustomer.Name + "<br>");
+        Response.Write("Password: " + ACustomer.Password + "<br>");
+        Response.Write("Date of Birth: " + ACustomer.DateOfBirth + "<br>");
+        Response.Write("Address: " + ACustomer.Address + "<br>");
+        Response.Write("Archived: " + ACustomer.Archived);
+    }
+
+    protected void btnBack_Click(object sender, EventArgs e)
+    {
+        //redirect to Customer List
+        Response.Redirect("CustomerList.aspx");
     }
 }
